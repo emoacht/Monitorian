69d16c4ac634136fdd5db7f7bf4808a17d5c085c
emoacht <emotom@pobox.com>
2017-02-06 01:10:10 +0900

    Added icon image data

---
+++ Source/IconImage/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+
+namespace IconImage
+{
+	public partial class MainWindow : Window
+	{
+		#region Property
+
+		public double ImageLength
+		{
+			get { return (double)GetValue(ImageLengthProperty); }
+			set { SetValue(ImageLengthProperty, value); }
+		}
+		public static readonly DependencyProperty ImageLengthProperty =
+			DependencyProperty.Register(
+				"ImageLength",
+				typeof(double),
+				typeof(MainWindow),
+				new PropertyMetadata(256D));
+
+		#endregion
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private string _fileName = "Icon.png";
+		private string _folderPath;
+
+		public void SaveImage()
+		{
+			var sfd = new SaveFileDialog
+			{
+				FileName = _fileName,
+				Filter = "*(.png)|*.png",
+				InitialDirectory = _folderPath ?? Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+			};
+
+			if (sfd.ShowDialog() != true)
+				return;
+
+			_fileName = Path.GetFileName(sfd.FileName);
+			_folderPath = Path.GetDirectoryName(sfd.FileName);
+
+			FrameworkElementImage.SaveImage(ImageContent, sfd.FileName, ImageLength, ImageLength);
+
+			SystemSounds.Asterisk.Play();
+		}
+	}
+}
---
+++ Source/IconImage/ScaleConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace IconImage
+{
+	[ValueConversion(typeof(double), typeof(double))]
+	public class ScaleConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			double factor;
+			if (!(value is double) || !double.TryParse(parameter.ToString(), out factor))
+				return DependencyProperty.UnsetValue;
+
+			return (double)value / factor;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			double factor;
+			if (!(value is double) || !double.TryParse(parameter.ToString(), out factor))
+				return DependencyProperty.UnsetValue;
+
+			return (double)value * factor;
+		}
+	}
+}

818384bd67a3d286e41a1696a391a549bfc17c11
emoacht <emotom@pobox.com>
2017-10-12 06:05:52 +0900

    Incremented build number

--- Source/Monitorian/Properties/AssemblyInfo.cs
+++ Source/Monitorian/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.3.0")]
-[assembly: AssemblyFileVersion("1.2.3.0")]
+[assembly: AssemblyVersion("1.2.4.0")]
+[assembly: AssemblyFileVersion("1.2.4.0")]
 [assembly: Guid("9e001d5b-6d18-48ba-a1d9-f1e2410a52d3")]
 

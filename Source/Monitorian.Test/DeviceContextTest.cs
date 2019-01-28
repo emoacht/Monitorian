44aba6750198d99253f3d0010dadb876cd716164
emoacht <emotom@pobox.com>
2019-01-29 06:35:14 +0900

    Added test cases

--- Source/Monitorian.Test/DeviceContextTest.cs
+++ Source/Monitorian.Test/DeviceContextTest.cs
@@ -11,6 +11,7 @@ namespace Monitorian.Test
 		[TestMethod]
 		public void TestGetDeviceInstanceId1()
 		{
+			// Surface Display
 			var source = @"\\?\DISPLAY#SDC3853#4&3b1d693f&0&UID265988#{e6f07b5f-ee97-4a90-b076-33f57bf4eaa7}";
 			var expected = @"DISPLAY\SDC3853\4&3b1d693f&0&UID265988";
 
@@ -20,6 +21,7 @@ namespace Monitorian.Test
 		[TestMethod]
 		public void TestGetDeviceInstanceId2()
 		{
+			// Dell U2415
 			var source = @"\\?\DISPLAY#DELA0B8#4&3b1d693f&0&UID224795#{e6f07b5f-ee97-4a90-b076-33f57bf4eaa7}";
 			var expected = @"DISPLAY\DELA0B8\4&3b1d693f&0&UID224795";
 
--- Source/Monitorian.Test/Properties/AssemblyInfo.cs
+++ Source/Monitorian.Test/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Monitorian.Test")]
-[assembly: AssemblyCopyright("Copyright © 2018 emoacht")]
+[assembly: AssemblyCopyright("Copyright © 2018-2019 emoacht")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("338b3627-9128-45cf-92c2-86b04318c24b")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.0.0")]
-[assembly: AssemblyFileVersion("1.8.0.0")]
+[assembly: AssemblyVersion("1.10.0.0")]
+[assembly: AssemblyFileVersion("1.10.0.0")]

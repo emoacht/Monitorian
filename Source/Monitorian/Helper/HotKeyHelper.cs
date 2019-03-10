e8c3b40cc5c830f5212b595549416c31cf0cb18c
りふれ <reflecat@icloud.com>
2019-03-10 22:04:21 +0900

    Add system hot key helper routines and a hot key message listener

---
+++ Source/Monitorian/Helper/HotKeyHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Monitorian.Helper
+{
+	internal static class HotKeyHelper
+	{
+		#region Win32
+
+		[DllImport("User32.dll", SetLastError = true)]
+		internal static extern bool RegisterHotKey(
+			IntPtr hWnd,
+			int id,
+			KeyModifiers fsModifiers,
+			System.Windows.Forms.Keys vk);
+
+		[Flags]
+		internal enum KeyModifiers : uint
+		{
+			MOD_ALT = 0x0001,
+			MOD_CONTROL = 0x0002,
+			MOD_SHIFT = 0x0004,
+			MOD_WIN = 0x0008,
+			MOD_NOREPEAT = 0x4000,
+		}
+
+		[DllImport("User32.dll", SetLastError = true)]
+		internal static extern bool UnregisterHotKey(
+			IntPtr hWnd,
+			int id);
+
+		internal const int WM_HOTKEY = 0x0312;
+		internal const int IDHOT_SNAPDESKTOP = -2;
+		internal const int IDHOT_SNAPWINDOW = -1;
+
+		#endregion
+	}
+
+	internal class HotKeyTuple
+	{
+		public int Id { get; set; }
+		public System.Windows.Forms.Keys VirtualKey { get; set; }
+		public HotKeyHelper.KeyModifiers KeyModifiers { get; set; }
+	}
+
+	internal class HotKeyEventArgs : EventArgs
+	{
+		public HotKeyEventArgs(HotKeyTuple args)
+		{
+			_args = args;
+		}
+
+		private readonly HotKeyTuple _args;
+
+		public int Id { get => _args.Id; }
+		public System.Windows.Forms.Keys VirtualKey { get => _args.VirtualKey; }
+		public HotKeyHelper.KeyModifiers KeyModifiers { get => _args.KeyModifiers; }
+	}
+
+	internal delegate void HotKeyEventHandler(object sender, HotKeyEventArgs e);
+}
---
+++ Source/Monitorian/HotKeyListener.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+using Monitorian.Models.Monitor;
+using Monitorian.Helper;
+
+namespace Monitorian
+{
+	class HotKeyListener : IDisposable
+	{
+		public enum HotKeyId : int
+		{
+			IncrementBrightness,
+			DecrementBrightness
+		}
+
+		private readonly HotKeyForm _listenerForm;
+
+		/// <summary>
+		/// Occurs when a hot key registerd by the RegisterHotKey function is pressed.
+		/// </summary>
+		public event HotKeyEventHandler HotKeyDown;
+
+		public HotKeyListener()
+		{
+			var requests = new List<HotKeyTuple>();
+			requests.Add(new HotKeyTuple
+			{
+				Id = (int)HotKeyId.IncrementBrightness,
+				KeyModifiers = HotKeyHelper.KeyModifiers.MOD_WIN,
+				VirtualKey = Keys.F9
+			});
+			requests.Add(new HotKeyTuple
+			{
+				Id = (int)HotKeyId.DecrementBrightness,
+				KeyModifiers = HotKeyHelper.KeyModifiers.MOD_WIN,
+				VirtualKey = Keys.F8
+			});
+
+			_listenerForm = new HotKeyForm(requests, this);
+		}
+
+		#region IDisposable
+
+		private bool _isDisposed = false;
+
+		/// <summary>
+		/// Public implementation of Dispose pattern
+		/// </summary>
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary>
+		/// Protected implementation of Dispose pattern
+		/// </summary>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (_isDisposed)
+				return;
+
+			if (disposing)
+			{
+				_listenerForm?.Dispose();
+			}
+
+			_isDisposed = true;
+		}
+
+		#endregion
+
+		private class HotKeyForm : Form
+		{
+			private List<HotKeyTuple> _hotKeys;
+			private HotKeyListener _ownerListener;
+
+			public HotKeyForm(List<HotKeyTuple> hotKeys, HotKeyListener ownerListener)
+			{
+				_hotKeys = hotKeys;
+				_ownerListener = ownerListener;
+
+				foreach (var hotKey in hotKeys)
+				{
+					var result = HotKeyHelper.RegisterHotKey(this.Handle, hotKey.Id, hotKey.KeyModifiers, hotKey.VirtualKey);
+					if (!result)
+					{
+						Debug.WriteLine($"Failed to register the hot key. {Error.CreateMessage()}");
+					}
+				}
+			}
+
+			protected override void WndProc(ref Message m)
+			{
+				switch (m.Msg)
+				{
+					case HotKeyHelper.WM_HOTKEY:
+						var hotkeyTuple = new HotKeyTuple
+						{
+							Id = (int)m.WParam,
+							VirtualKey = (Keys)Enum.ToObject(typeof(Keys), unchecked((short)(uint)m.WParam >> 16)),
+							KeyModifiers = (HotKeyHelper.KeyModifiers)Enum.ToObject(typeof(HotKeyHelper.KeyModifiers), unchecked((short)m.WParam))
+						};
+						_ownerListener.HotKeyDown?.Invoke(this, new HotKeyEventArgs(hotkeyTuple));
+						break;
+				}
+
+				base.WndProc(ref m);
+			}
+
+			protected override void Dispose(bool disposing)
+			{
+				foreach (var hotKey in _hotKeys)
+				{
+					var result = HotKeyHelper.UnregisterHotKey(this.Handle, hotKey.Id);
+					if (!result)
+					{
+						Debug.WriteLine($"Failed to unregister the hot key. {Error.CreateMessage()}");
+					}
+				}
+			}
+		}
+	}
+}

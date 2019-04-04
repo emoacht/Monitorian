15413a6e1a13914c543f9077d5fb20d3e86f6bf7
emoacht <emotom@pobox.com>
2019-04-04 20:26:44 +0900

    Add variation of boolean to visibility converter

---
+++ Source/Monitorian.Core/Views/Converters/BooleanInverseToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Monitorian.Core.Views.Converters
+{
+	[ValueConversion(typeof(bool), typeof(Visibility))]
+	public class BooleanInverseToVisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (!(value is bool sourceValue))
+				return DependencyProperty.UnsetValue;
+
+			return !sourceValue ? Visibility.Visible : Visibility.Collapsed;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (!(value is Visibility sourceValue))
+				return DependencyProperty.UnsetValue;
+
+			return !(sourceValue == Visibility.Visible);
+		}
+	}
+}

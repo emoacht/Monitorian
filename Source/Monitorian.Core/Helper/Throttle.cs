bb3335a6dc3b026707aab0f9b9aafeebbab39b95
emoacht <emotom@pobox.com>
2019-04-30 23:01:21 +0900

    Fix Throttle operator

--- Source/Monitorian.Core/Helper/Throttle.cs
+++ Source/Monitorian.Core/Helper/Throttle.cs
@@ -13,26 +13,29 @@ namespace Monitorian.Core.Helper
 	/// </summary>
 	public class Throttle
 	{
-		protected readonly Action _action;
-		protected readonly DispatcherTimer _timer;
 		protected readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+		protected readonly DispatcherTimer _timer;
+		protected readonly Action _action;
 
 		public Throttle(TimeSpan dueTime, Action action)
 		{
 			if (dueTime <= TimeSpan.Zero)
 				throw new ArgumentOutOfRangeException(nameof(dueTime));
 
-			this._action = action;
-
 			_timer = new DispatcherTimer { Interval = dueTime };
 			_timer.Tick += OnTick;
+
+			this._action = action;
 		}
 
-		private void OnTick(object sender, EventArgs e)
+		private async void OnTick(object sender, EventArgs e)
 		{
 			try
 			{
-				_semaphore.Wait();
+				await _semaphore.WaitAsync();
+
+				if (!_timer.IsEnabled)
+					return;
 
 				_timer.Stop();
 				_action?.Invoke();

840a605387916b4aa5e6b722bef696e583c0a24b
emoacht <emotom@pobox.com>
2019-05-15 22:38:36 +0900

    Increment major version

--- Source/Monitorian.Test/Properties/AssemblyInfo.cs
+++ Source/Monitorian.Test/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("338b3627-9128-45cf-92c2-86b04318c24b")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.10.0.0")]
-[assembly: AssemblyFileVersion("1.10.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]

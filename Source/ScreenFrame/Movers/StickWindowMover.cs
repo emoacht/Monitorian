724110924baf0b135ab714b3b8f4730ff2418548
emoacht <emotom@pobox.com>
2019-10-13 19:32:48 +0900

    Modify method to get adjacent location to avoid overlap with notification area

--- Source/ScreenFrame/Movers/StickWindowMover.cs
+++ Source/ScreenFrame/Movers/StickWindowMover.cs
@@ -40,6 +40,24 @@ namespace ScreenFrame.Movers
 		protected override bool TryGetAdjacentLocation(double windowWidth, double windowHeight, out Rect location) =>
 			TryGetAdjacentLocationToTaskbar(windowWidth, windowHeight, out location);
 
+		private enum IconPlacement
+		{
+			/// <summary>
+			/// The location of NotifyIcon is unknown.
+			/// </summary>
+			Unknown = 0,
+
+			/// <summary>
+			/// NotifyIcon locates in the taskbar.
+			/// </summary>
+			InTaskbar,
+
+			/// <summary>
+			/// NotifyIcon locates in the notification overflow area.
+			/// </summary>
+			InOverflowArea
+		}
+
 		/// <summary>
 		/// Attempts to get the adjacent location to NotifyIcon using specified window width and height.
 		/// </summary>
@@ -55,20 +73,43 @@ namespace ScreenFrame.Movers
 				return false;
 			}
 
-			if (!NotifyIconHelper.TryGetNotifyIconRect(_notifyIcon, out Rect iconRect))
-				iconRect = taskbarRect; // Fallback
+			var iconPlacement = IconPlacement.Unknown;
+			var overflowAreaRect = default(Rect);
+
+			if (NotifyIconHelper.TryGetNotifyIconRect(_notifyIcon, out Rect iconRect))
+			{
+				if (taskbarRect.Contains(iconRect))
+				{
+					iconPlacement = IconPlacement.InTaskbar;
+				}
+				else if (WindowHelper.TryGetOverflowAreaRect(out overflowAreaRect))
+				{
+					iconPlacement = IconPlacement.InOverflowArea;
+				}
+			}
 
 			if (!WindowHelper.TryGetDwmWindowMargin(_window, out Thickness windowMargin))
 				windowMargin = new Thickness(0); // Fallback
 
-			double x = 0;
-			double y = 0;
+			double x = 0, y = 0;
 
 			switch (taskbarAlignment)
 			{
 				case TaskbarAlignment.Top:
 				case TaskbarAlignment.Bottom:
-					x = iconRect.Right - windowWidth + windowMargin.Right;
+					switch (iconPlacement)
+					{
+						case IconPlacement.InTaskbar:
+							x = iconRect.Right;
+							break;
+						case IconPlacement.InOverflowArea:
+							x = overflowAreaRect.Left;
+							break;
+						default:
+							x = taskbarRect.Right; // Fallback
+							break;
+					}
+					x -= (windowWidth - windowMargin.Right);
 
 					switch (taskbarAlignment)
 					{
@@ -77,7 +118,7 @@ namespace ScreenFrame.Movers
 							PivotAlignment = PivotAlignment.TopRight;
 							break;
 						case TaskbarAlignment.Bottom:
-							y = taskbarRect.Top - windowHeight + windowMargin.Bottom;
+							y = taskbarRect.Top - (windowHeight - windowMargin.Bottom);
 							PivotAlignment = PivotAlignment.BottomRight;
 							break;
 					}
@@ -91,12 +132,24 @@ namespace ScreenFrame.Movers
 							PivotAlignment = PivotAlignment.BottomLeft;
 							break;
 						case TaskbarAlignment.Right:
-							x = taskbarRect.Left - windowWidth + windowMargin.Right;
+							x = taskbarRect.Left - (windowWidth - windowMargin.Right);
 							PivotAlignment = PivotAlignment.BottomRight;
 							break;
 					}
 
-					y = iconRect.Bottom - windowHeight + windowMargin.Bottom;
+					switch (iconPlacement)
+					{
+						case IconPlacement.InTaskbar:
+							y = iconRect.Bottom;
+							break;
+						case IconPlacement.InOverflowArea:
+							y = overflowAreaRect.Top;
+							break;
+						default:
+							y = taskbarRect.Bottom; // Fallback
+							break;
+					}
+					y -= (windowHeight - windowMargin.Bottom);
 					break;
 			}
 			location = new Rect(x, y, windowWidth, windowHeight);

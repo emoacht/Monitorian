01e923bd7db2d1a89800e6b08d42a04b87248a3a
emoacht <emotom@pobox.com>
2020-07-31 23:42:02 +0900

    Increment minor version

--- Source/Monitorian.Core/Properties/AssemblyInfo.cs
+++ Source/Monitorian.Core/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Monitorian.Core")]
-[assembly: AssemblyCopyright("Copyright © 2019 emoacht")]
+[assembly: AssemblyCopyright("Copyright © 2020 emoacht")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
 // For unit test

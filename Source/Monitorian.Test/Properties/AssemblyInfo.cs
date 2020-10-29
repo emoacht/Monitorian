f9efad787b779105be0c951dbf9d27ad56dd8dd8
emoacht <emotom@pobox.com>
2020-10-30 07:30:29 +0900

    Increment minor version

--- Source/Monitorian.Test/Properties/AssemblyInfo.cs
+++ Source/Monitorian.Test/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Monitorian.Test")]
-[assembly: AssemblyCopyright("Copyright © 2018-2019 emoacht")]
+[assembly: AssemblyCopyright("Copyright © 2018-2020 emoacht")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("338b3627-9128-45cf-92c2-86b04318c24b")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.7.0.0")]
+[assembly: AssemblyFileVersion("2.7.0.0")]

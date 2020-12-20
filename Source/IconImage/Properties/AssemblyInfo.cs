354c8c194ce2098cf8461f93550b1816a8f139c9
emoacht <emotom@pobox.com>
2020-12-21 07:50:11 +0900

    Increment minor version

--- Source/IconImage/Properties/AssemblyInfo.cs
+++ Source/IconImage/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("IconImage")]
-[assembly: AssemblyCopyright("Copyright © 2017-2019 emoacht")]
+[assembly: AssemblyCopyright("Copyright © 2017 emoacht")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.9.0.0")]
+[assembly: AssemblyFileVersion("2.9.0.0")]

119f4539275c6d30c9ad88ef731a972084920901
emoacht <emotom@pobox.com>
2021-01-23 12:10:58 +0900

    Add generic Throttle like operator

--- Source/Monitorian.Core/Helper/Throttle.cs
+++ Source/Monitorian.Core/Helper/Throttle.cs
@@ -67,6 +67,67 @@ namespace Monitorian.Core.Helper
 	}
 
 	/// <summary>
+	/// Rx Throttle like operator which holds generic objects in a queue
+	/// </summary>
+	public class Throttle<T>
+	{
+		protected readonly SemaphoreSlim _semaphore = new(1, 1);
+		protected readonly DispatcherTimer _timer;
+		protected readonly Queue<T> _queue = new();
+		protected readonly Action<T[]> _action;
+
+		public Throttle(TimeSpan dueTime, Action<T[]> action)
+		{
+			if (dueTime <= TimeSpan.Zero)
+				throw new ArgumentOutOfRangeException(nameof(dueTime), dueTime, "The time must be positive.");
+
+			_timer = new(
+				interval: dueTime,
+				priority: DispatcherPriority.Background,
+				callback: OnTick,
+				dispatcher: Application.Current.Dispatcher);
+
+			this._action = action;
+		}
+
+		private async void OnTick(object sender, EventArgs e)
+		{
+			try
+			{
+				await _semaphore.WaitAsync();
+
+				if (!_timer.IsEnabled)
+					return;
+
+				_timer.Stop();
+				_action?.Invoke(_queue.ToArray());
+				_queue.Clear();
+
+			}
+			finally
+			{
+				_semaphore.Release();
+			}
+		}
+
+		public virtual async Task PushAsync(T item)
+		{
+			try
+			{
+				await _semaphore.WaitAsync();
+
+				_timer.Stop();
+				_queue.Enqueue(item);
+				_timer.Start();
+			}
+			finally
+			{
+				_semaphore.Release();
+			}
+		}
+	}
+
+	/// <summary>
 	/// Rx Sample like operator
 	/// </summary>
 	public class Sample : Throttle

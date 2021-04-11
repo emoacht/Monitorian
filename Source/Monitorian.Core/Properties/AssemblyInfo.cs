36cf9316cbb6af3b9a97f64fdcb38f7274790af5
emoacht <emotom@pobox.com>
2021-04-11 16:44:27 +0900

    Increment minor version

--- Source/Monitorian.Core/Properties/AssemblyInfo.cs
+++ Source/Monitorian.Core/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.15.0.0")]
-[assembly: AssemblyFileVersion("2.15.0.0")]
+[assembly: AssemblyVersion("2.16.0.0")]
+[assembly: AssemblyFileVersion("2.16.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
 // For unit test

789604d825af7cb9c6845568a5a3f19a13abcbc6
emoacht <emotom@pobox.com>
2021-08-27 21:17:41 +0900

    Increase right padding of last visible button

--- Source/Monitorian.Core/Views/Controls/IconButton.cs
+++ Source/Monitorian.Core/Views/Controls/IconButton.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -12,6 +13,8 @@ namespace Monitorian.Core.Views.Controls
 {
 	public class IconButton : ToggleButton
 	{
+		#region Icon
+
 		public Geometry StrokeIconData
 		{
 			get { return (Geometry)GetValue(StrokeIconDataProperty); }
@@ -35,6 +38,73 @@ namespace Monitorian.Core.Views.Controls
 				typeof(Geometry),
 				typeof(IconButton),
 				new PropertyMetadata(null));
+
+		#endregion
+
+		#region Visibility
+
+		private class IconButtonGroup
+		{
+			private readonly IconButton[] _buttons;
+
+			public IconButtonGroup(IEnumerable<IconButton> buttons)
+			{
+				_buttons = buttons.Reverse().ToArray();
+
+				CheckVisibleButtons();
+
+				foreach (var b in _buttons)
+					b.IsVisibleChanged += (_, _) => CheckVisibleButtons();
+			}
+
+			private void CheckVisibleButtons()
+			{
+				var isVisibleButtonFound = false;
+
+				foreach (var b in _buttons)
+				{
+					if (!isVisibleButtonFound && b.IsVisible)
+					{
+						isVisibleButtonFound = true;
+						b.IsLastVisibleButton = true;
+					}
+					else
+					{
+						b.IsLastVisibleButton = false;
+					}
+				}
+			}
+		}
+
+		private IconButtonGroup _group;
+
+		public override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			if (this.Parent is Panel panel)
+			{
+				var buttons = panel.Children.OfType<IconButton>();
+				if (buttons.All(x => x._group is null))
+				{
+					this._group = new IconButtonGroup(buttons);
+				}
+			}
+		}
+
+		public bool IsLastVisibleButton
+		{
+			get { return (bool)GetValue(IsLastVisibleButtonProperty); }
+			set { SetValue(IsLastVisibleButtonProperty, value); }
+		}
+		public static readonly DependencyProperty IsLastVisibleButtonProperty =
+			DependencyProperty.Register(
+				"IsLastVisibleButton",
+				typeof(bool),
+				typeof(IconButton),
+				new PropertyMetadata(false));
+
+		#endregion
 
 		private UIElement _parent;
 

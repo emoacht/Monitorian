c528f2798d53f830c6fa85eb340af07a6fdb992f
emoacht <emotom@pobox.com>
2021-12-23 21:28:44 +0900

    Increment build number

--- Source/Monitorian.Supplement/Properties/AssemblyInfo.cs
+++ Source/Monitorian.Supplement/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.5.0.0")]
-[assembly: AssemblyFileVersion("3.5.0.0")]
+[assembly: AssemblyVersion("3.8.2.0")]
+[assembly: AssemblyFileVersion("3.8.2.0")]
 [assembly: NeutralResourcesLanguage("en-US")]

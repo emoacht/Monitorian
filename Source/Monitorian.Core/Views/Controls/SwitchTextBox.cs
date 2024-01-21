0723c6a96899f031bdfb9cd6ba41d939a03ab91d
emoacht <emotom@pobox.com>
2024-01-21 11:31:04 +0900

    Further modify to release DispatcherTimer

--- Source/Monitorian.Core/Views/Controls/SwitchTextBox.cs
+++ Source/Monitorian.Core/Views/Controls/SwitchTextBox.cs
@@ -32,28 +32,37 @@ public class SwitchTextBox : TextBox
 	private InputDevice _device;
 	private Point _startPosition;
 	private bool _isContextMenuOpenable = true;
-	private DispatcherTimer _switchTimer;
+	private DispatcherTimer _timer;
+	private Window _window;
 
 	protected override void OnInitialized(EventArgs e)
 	{
 		base.OnInitialized(e);
 
-		var window = Window.GetWindow(this);
-		if (window is not null)
-		{
-			window.Closed += OnClosed;
-		}
+		this.Unloaded += OnUnloaded;
 
-		void OnClosed(object sender, EventArgs e)
-		{
-			var window = (Window)sender;
-			window.Closed -= OnClosed;
+		_window = Window.GetWindow(this);
+		if (_window is not null)
+			_window.Closed += OnClosed;
+	}
 
-			if (_switchTimer is not null)
-			{
-				_switchTimer.Stop();
-				_switchTimer.Tick -= OnTick;
-			}
+	private void OnUnloaded(object sender, RoutedEventArgs e)
+	{
+		// Window.GetWindow method will not return the hosting window when unloaded.
+
+		if (_window is not null)
+			OnClosed(_window, e);
+	}
+
+	private void OnClosed(object sender, EventArgs e)
+	{
+		((Window)sender).Closed -= OnClosed;
+		_window = null;
+
+		if (_timer is not null)
+		{
+			_timer.Stop();
+			_timer.Tick -= OnTick;
 		}
 	}
 
@@ -68,20 +77,20 @@ public class SwitchTextBox : TextBox
 
 		this._isContextMenuOpenable = isContextMenuOpenable;
 
-		_switchTimer ??= new DispatcherTimer(HoldingDuration, DispatcherPriority.Background, OnTick, Dispatcher.CurrentDispatcher);
-		_switchTimer.Start();
+		_timer ??= new DispatcherTimer(HoldingDuration, DispatcherPriority.Background, OnTick, Dispatcher.CurrentDispatcher);
+		_timer.Start();
 	}
 
 	private void OnDeviceUp()
 	{
-		_switchTimer?.Stop();
+		_timer?.Stop();
 
 		_device = null;
 	}
 
 	private void OnTick(object sender, EventArgs e)
 	{
-		_switchTimer.Stop();
+		_timer.Stop();
 
 		if (!TryGetDevicePosition(_device, out Point endPosition))
 			return;
@@ -125,11 +134,10 @@ public class SwitchTextBox : TextBox
 		// If a TextBox has focus when the window is deactivated, LostFocus event will occur
 		// after Window.Deactivated event. Since a TextBox's text will be updated to source when
 		// LostFocus event occurs by default, Window.Deactivated event is not always appropriate
-		// to capture the latest text.
+		// to update the latest text.
 
-		var window = Window.GetWindow(this);
-		if (!window.IsActive)
-			_switchTimer?.Stop();
+		if (_window is { IsActive: false })
+			_timer?.Stop();
 
 		this.IsReadOnly = true;
 

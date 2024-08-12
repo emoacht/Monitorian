616cd0d5cfcba52ff213547d23835ae0c3a054d8
emoacht <emotom@pobox.com>
2024-08-12 14:22:07 +0900

    Modify method to get system information

--- Source/Monitorian.Core/Models/SystemInfo.cs
+++ Source/Monitorian.Core/Models/SystemInfo.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Linq;
 using System.Management;
+using System.Runtime.InteropServices;
 using Microsoft.Win32;
 
 namespace Monitorian.Core.Models;
@@ -18,26 +19,36 @@ public static class SystemInfo
 
 		public SystemInfoInternal()
 		{
-			try
-			{
-				using var @class = new ManagementClass("Win32_ComputerSystem");
-				using var instance = @class.GetInstances().Cast<ManagementObject>().FirstOrDefault();
-
-				Manufacturer = instance?["Manufacturer"] as string;
-				Model = instance?["Model"] as string;
-				return;
-			}
-			catch (ManagementException)
-			{
-				Debug.WriteLine($"Failed to get instances by Win32_ComputerSystem");
-			}
-
 			const string keyName = @"SYSTEM\CurrentControlSet\Control\SystemInformation"; // HKLM
 
 			using var key = Registry.LocalMachine.OpenSubKey(keyName);
 
 			Manufacturer = key?.GetValue("SystemManufacturer") as string;
 			Model = key?.GetValue("SystemProductName") as string;
+
+			// Fallback
+			if (string.IsNullOrEmpty(Manufacturer) ||
+				string.IsNullOrEmpty(Model))
+			{
+				try
+				{
+					using var @class = new ManagementClass("Win32_ComputerSystem");
+					using var instance = @class.GetInstances().Cast<ManagementObject>().FirstOrDefault();
+
+					Manufacturer = instance?["Manufacturer"] as string;
+					Model = instance?["Model"] as string;
+				}
+				catch (ManagementException)
+				{
+					Debug.WriteLine("Failed to get instances by Win32_ComputerSystem.");
+				}
+				catch (COMException ce) when ((uint)ce.HResult is 0x80070422)
+				{
+					// Error message: The service cannot be started, either because it is disabled or because it has no enabled devices associated with it.
+					// Error code: 0x80070422
+					// This error occurs when WMI is disabled.
+				}
+			}
 		}
 	}
 

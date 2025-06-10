c4d64e05f5dffe8f1ddc14b708bd1ae9417f2aa4
emoacht <emotom@pobox.com>
2025-06-11 03:46:03 +0900

    Increment version to 4.11.1

--- Source/Monitorian.Core/Properties/AssemblyInfo.cs
+++ Source/Monitorian.Core/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.11.0.0")]
-[assembly: AssemblyFileVersion("4.11.0.0")]
+[assembly: AssemblyVersion("4.11.1.0")]
+[assembly: AssemblyFileVersion("4.11.1.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
 // For unit test
